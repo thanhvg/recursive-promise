5b636b82f968f9c1bfab2673c367a2bba1d76c9e
Thanh <thanhvg@gmail.com>
2018-05-09 19:51:09 -0600

    wont work because the init lock will be used to all events

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-
-
 const asyncAction = (a) => new Promise((resolve, reject) => {
   setTimeout(() => { console.log('asyncAction returns:', a);
                      resolve(a);
-                   }, 200);
+                   }, 1000);
 })
 
 // asyncAction('yay')
 
-let promiseLock = new Promise.resolve();
+let promiseLock = Promise.resolve();
 
-const getAsyncData = (a) => asyncAction(a).then((aa) => console.log('getAsyncData returns', aa))
+const instantLock = () => {
+};
 
-myEmitter.on('event', (a, b) => {
-  console.log(a, b, this);
-  // Prints: a b {}
-});
+// assign in then clause is not instant because then
+const getAsyncData = (a) => promiseLock.then(() => { promiseLock = asyncAction(a).then((aa) => console.log('getAsyncData returns', aa)); return promiseLock; })
+// const getAsyncData = (a) => { promiseLock = asyncAction(a).then((aa) => console.log('getAsyncData returns', aa)); return promiseLock; }
+
+myEmitter.on('event', getAsyncData);
 
 
-myEmitter.emit('event', 'a', 'b');
+myEmitter.emit('event', 'a');
+myEmitter.emit('event', 'b');
+myEmitter.emit('event', 'c');
+
+setTimeout(() => myEmitter.emit('event', 'd'), 20)
+setTimeout(() => myEmitter.emit('event', 'e'), 20)
